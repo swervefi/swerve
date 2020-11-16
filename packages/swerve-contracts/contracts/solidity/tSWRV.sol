b29505ec52ddc2bee936a93a05844013c63a9238
Grant Steuart <gmsteuart@gmail.com>
2020-11-16 13:14:01 -0700

    fix: oz contract imports

--- packages/swerve-contracts/contracts/solidity/tSWRV.sol
+++ packages/swerve-contracts/contracts/solidity/tSWRV.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.6.10;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 /**
  * @dev Implementation of the SIP-12 : "tSWRV - Treasury Promissory Tokens".
