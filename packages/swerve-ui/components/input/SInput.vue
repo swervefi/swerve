92022c890d63093e93fb6f352c9964dbdc01714f
Grant Steuart <gmsteuart@gmail.com>
2021-01-03 17:58:07 -0600

    refactor: shorten component names

--- packages/swerve-ui/components/input/SInput.vue
+++ packages/swerve-ui/components/input/SInput.vue
@@ -1,27 +1,39 @@
 <template>
-  <div class="input" :class="{ required: 'input--required' }">
+  <div class="field" :class="{ required }">
     <input type="text" :value="value" :id="name" :aria-required="required" />
     <label :for="name">
       {{ label }}
     </label>
+    <SToken />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
+import SToken from '@/components/base/SToken.vue'
 
 export default defineComponent({
   name: 'SInput',
   inheritAttrs: false,
   props: {
-    required: {
-      type: Boolean,
-      default: false
-    },
+    /**
+     * Disables the input
+     */
+    disabled: Boolean,
+    /**
+     * Input label to display
+     */
     label: {
       type: String,
-      default: () => ""
-    }
+      required: true
+    },
+    /**
+     * Forces user to supply input
+     */
+    required: Boolean
+  },
+  components: {
+    SToken
   },
   computed: {
     name(): String {
@@ -37,12 +49,26 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+.field {
+  display: flex;
+  flex-flow: column-reverse;
+  align-items: start;
+}
+
 input {
-  background: #2f3437;
-  padding: 4px;
+  background: rgba(18, 20, 21, 1);
+  padding: 0.25em;
   border: none;
-  border-radius: 4px;
-  color: rgba(134, 252, 231, 1);
+  border-radius: 0.25em;
+  color: rgba(240, 240, 240, 1); // Initial state
+  height: 3.5em;
+
+  &.cyberpunk {
+    color: rgba(134, 252, 231, 1);
+  }
+
+  &.required {
+  }
 
   &:disabled {
     background: rgba($color: #2f3437, $alpha: 0.6);
@@ -51,8 +77,22 @@ input {
   }
 
   &:focus {
+    color: rgba(255, 255, 255, 1);
     border: 1px solid rgba(174, 252, 251, 1);
     outline: none;
+
+    &.cyberpunk {
+      box-shadow: 0px 0px 12px 0px rgba(134, 252, 231, 1);
+    }
   }
+}
+
+label {
+  font-size: 0.875em;
+  line-height: 1.125em;
+  margin-bottom: 0.25em;
+}
+
+input + label {
 }
 </style>
